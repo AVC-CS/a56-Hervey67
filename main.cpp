cf633977a3efb02f526c080e3cbd78bd5d2b1a08
Hervey67 <herv2007@gmail.com>
2026-03-28 23:25:02 -0700

    Changes to main.cpp

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@ int main()
 {
 	int N;
 	cin >> N;
-	for (int i = 0; i<N; i++){
-    	for (int j = 0; j<i; j++){
+	for (int i = 0; i<=N - 1; i++){
+    	for (int j = N - 1 - i; j<N - 1; j++){
       	cout << i << "," << j << " ";
     	}
     	cout << endl;
